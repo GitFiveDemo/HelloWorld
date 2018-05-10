2569f3f68a70f3472207d079f9b9b7530bcdd75e
Administrator <MincingMachine>
2018-05-10 14:05:11 +0800

    gradle

--- app/src/main/java/com/jinou/app/MainActivity.java
+++ app/src/main/java/com/jinou/app/MainActivity.java
@@ -9,5 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        *老辛
+        *老石
+        *老许
+        *老王
+        * */
     }
 }
