54624dd369e464476f232882c7918a55864ffc1a
chenguang071116 <294633912@qq.com>
2018-05-10 16:12:26 +0800

    update framework

--- app/src/main/java/com/jinou/app/view/activity/MainActivity.java
+++ app/src/main/java/com/jinou/app/view/activity/MainActivity.java
@@ -1,20 +1,15 @@
-package com.jinou.app;
+package com.jinou.app.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jinou.app.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        *老辛
-        *老石
-        *老许
-        *老王
-        *老胖
-        * 开会啦！
-        * */
+
     }
 }
