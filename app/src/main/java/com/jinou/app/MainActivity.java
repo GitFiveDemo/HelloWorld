997e725e0b541ed1b56017d3b1289c7425e970fb
Administrator <MincingMachine>
2018-05-10 13:54:45 +0800

    gradle

--- app/src/main/java/com/jinou/app/MainActivity.java
+++ app/src/main/java/com/jinou/app/MainActivity.java
@@ -5,23 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(TAG, "onCreate: ");
-        Log.d(TAG, "onCreate: ");
-        /**
-         ** .ignore插件加上Project打不开
-         *众里寻他千百度
-         *众里寻他千百度
-         * 众里寻他千百度
-         众里寻他千百度
-         *
-         *
-         * .ignore插件加上Project打不开
-         */
     }
 }
