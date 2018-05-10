3c086dc9680184b0e371fe3ccfd0328db0c8fae1
Administrator <MincingMachine>
2018-05-10 14:13:00 +0800

    gradle

--- app/src/main/java/com/jinou/app/MainActivity.java
+++ app/src/main/java/com/jinou/app/MainActivity.java
@@ -16,24 +16,4 @@ public class MainActivity extends AppCompatActivity {
         *老王
         * */
     }
-<<<<<<< HEAD
-
-        Log.d(TAG, "onCreate: ");
-        Log.d(TAG, "onCreate: ");
-
-        /**
-         * I am the hope of the whole village
-         * By the window
-         */
-=======
->>>>>>> 997e725e0b541ed1b56017d3b1289c7425e970fb
-
-	/*
-
-	悲哀是真的，泪是假的，没有什么可执着；一百年前，你不认识我，我不认识你；
-	背影是真的，人是假的，本没有因果；一百年后，没有你，也没有我。
-	欣赏与深思 悲哀和眼泪都是真的，区别只是内在和外在，无形和有形。
-	*/
-
-	}
 }
