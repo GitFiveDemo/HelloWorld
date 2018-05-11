fc2fc5009db3f3fcee1bc0dfd18ba4d4b5274f73
chenguang071116 <294633912@qq.com>
2018-05-11 13:58:29 +0800

    更新application 添加屏幕适配

--- app/src/main/java/com/jinou/app/application/CGApplication.java
+++ app/src/main/java/com/jinou/app/application/CGApplication.java
@@ -2,6 +2,9 @@ package com.jinou.app.application;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.view.WindowManager;
 
 /**
  * author: ShiChenguang
@@ -9,14 +12,28 @@ import android.content.Context;
  */
 public class CGApplication extends Application {
 
+    private static final int DESIGN_WIDTH = 280;
     private static Context context;
     @Override
     public void onCreate() {
         super.onCreate();
         context = this;
+        resetDensity();
     }
 
     public static Context getContext() {
         return context;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        resetDensity();//这个方法重写也是很有必要的
+    }
+
+    public void resetDensity(){
+        Point size = new Point();
+        ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getSize(size);
+        getResources().getDisplayMetrics().xdpi = size.x/DESIGN_WIDTH*72f;
     }
 }
