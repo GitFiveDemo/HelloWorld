212afcff71f41ca29123f7899c7b093c01c9ef03
Administrator <MincingMachine>
2018-05-11 21:18:49 +0800

    qqq

---
+++ app/src/main/java/com/jinou/app/view/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.jinou.app.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jinou.app.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
